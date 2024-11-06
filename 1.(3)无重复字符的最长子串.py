8775ef4f99fa27031fc24e3586a6c97d0fff52a1
现阶段：秀发 <1733135862@qq.com>
2024-11-06 23:59:22 +0800

    .

--- 1.(3)无重复字符的最长子串.py
+++ 1.(3)无重复字符的最长子串.py
@@ -1,4 +1,21 @@
 # 给定一个字符串 s ，请你找出其中不含有重复字符的 最长子串的长度
+import string
+
 
 class Solution:
     def lengthOfLongestSubstring(self, s):
+        step = len(s)
+        before = s[0]
+        last = s[1]
+        for location in range(step-1):
+            if before != last:
+                last =
+                sub_string = s[before:last+1]
+            # if before == last:
+            #     sub_string = s[before+1:last]
+        return sub_string
+
+solution = Solution()
+
+s = "abcdf"
+print(solution.lengthOfLongestSubstring(s))
