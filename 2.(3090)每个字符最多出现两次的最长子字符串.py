453147588434ef9cdd4979a8221561ccb8823bf5
现阶段：秀发 <1733135862@qq.com>
2024-11-08 16:59:15 +0800

    力扣3090. 每个字符最多出现两次的最长子字符串

--- 2.(3090)每个字符最多出现两次的最长子字符串.py
+++ 2.(3090)每个字符最多出现两次的最长子字符串.py
@@ -1,3 +1,27 @@
-# 给你一个字符串 s ，请找出满足每个字符最多出现两次的最长子字符串，并返回该子字符串的最大长度
 class Solution:
     def maximumLengthSubstring(self, s):
+        char_count = {}  # 记录窗口中每个字符的出现次数
+        left = 0  # 窗口的左边界
+        max_len = 0  # 记录满足条件的最长子串长度
+
+        for right in range(len(s)):
+            # 更新右边界字符的出现次数
+            char_count[s[right]] = char_count.get(s[right], 0) + 1
+
+            # 如果有字符出现次数超过两次，移动左边界
+            while char_count[s[right]] > 2:
+                char_count[s[left]] -= 1
+                if char_count[s[left]] == 0:
+                    del char_count[s[left]]
+                left += 1
+
+            # 更新最大长度
+            max_len = max(max_len, right - left + 1)
+
+        return max_len
+
+
+# 测试代码
+solution = Solution()
+s = "bcbbbcba"
+print(solution.maximumLengthSubstring(s))  # 输出应为 4
